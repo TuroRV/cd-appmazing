b6aefbbc8c4c24753eb2d776c17d8c9b5f7ae3c1
Arturo Raña <turo@hotmail.es>
2023-09-28 12:13:24 +0200

    add product controller /get

--- src/main/java/com/campusdual/appmazing/controller/ProductController.java
+++ src/main/java/com/campusdual/appmazing/controller/ProductController.java
@@ -1,10 +1,15 @@
 package com.campusdual.appmazing.controller;
 
+import com.campusdual.appmazing.api.IProductService;
+import com.campusdual.appmazing.model.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    IProductService productService;
     @GetMapping
     public String testController(){
         return "Product Controller works";
@@ -13,6 +18,16 @@ public class ProductController {
     @PostMapping
     public String testControllerBody(@RequestBody String name){
         return "Product Controller works, " + name + "!";
+    }
+    @GetMapping(value = "/testMethod")
+    public String testControllerMethod(){
+        return "Product controller method works!";
+    }
+
+    @PostMapping(value = "/get")
+    public ProductDTO queryProduct (@RequestBody ProductDTO product){
+        return this.productService.queryProduct(product);
+
     }
 
 }
