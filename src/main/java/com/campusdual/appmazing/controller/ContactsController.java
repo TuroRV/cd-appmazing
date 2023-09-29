f463dd542830f3dadcadc8f77685214149f2f4a6
Arturo Raña <turo@hotmail.es>
2023-09-29 08:10:06 +0200

    Se añade el controlador de contactos

--- src/main/java/com/campusdual/appmazing/controller/ContactsController.java
+++ src/main/java/com/campusdual/appmazing/controller/ContactsController.java
@@ -1,10 +1,11 @@
 package com.campusdual.appmazing.controller;
 
 import com.campusdual.appmazing.api.IContactsService;
+import com.campusdual.appmazing.model.dto.ContactsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/contacts")
@@ -14,6 +15,23 @@ public class ContactsController {
     @GetMapping
     public String testController(){
         return "Contacts Controller Works";
+    }
+
+    @PostMapping(value = "/get")
+    public ContactsDTO queryContacts (@RequestBody ContactsDTO contacts){
+        return this.contactsService.queryContacts(contacts);
+    }
+    @GetMapping(value = "/getAll")
+    public List<ContactsDTO> queryAllContacts(){
+        return this.contactsService.queryAllContacts();
+    }
+    @PostMapping(value = "/add")
+    public int insertContacts(@RequestBody ContactsDTO contacts){
+        return this.contactsService.insertContacts(contacts);
+    }
+    @PutMapping(value = "/update")
+    public int updateContacts (@RequestBody ContactsDTO contacts){
+        return this.contactsService.deleteContacts(contacts);
     }
 
 }
